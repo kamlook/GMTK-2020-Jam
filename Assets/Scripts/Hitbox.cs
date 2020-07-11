2026a0bdc231b285e3077bbf6173964c00438f53
DJ Uno <unodeej@gmail.com>
2020-07-10 18:53:27 -0700

    Press E to punch

---
+++ Assets/Scripts/Hitbox.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour
+{
+    void OnTriggerEnter(Collider c) {
+        this.transform.parent.GetComponent<Player>().PunchCollision(c);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Animator anim;
+
+    public GameObject punchHitbox;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+    }
+
+    void Attack() {
+        anim.SetTrigger("Attack");
+
+        // Spawn punch hitbox
+        punchHitbox.SetActive(true);
+    }
+
+    // This function is triggered at the "hit" portion of the anim
+    public void Hit() {
+      Debug.Log("YEA");
+
+      // Check overlap of punch hitbox
+
+      // Deactivate punch hitbox
+      punchHitbox.SetActive(false);
+    }
+
+    public void PunchCollision(Collider a_coll) {
+      Debug.Log(a_coll);
+      a_coll.gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E)) {
+            Attack();
+        }
     }
+
+
 }
